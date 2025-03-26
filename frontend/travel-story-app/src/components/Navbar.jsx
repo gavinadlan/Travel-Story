b502392580291e547561e59735a5b4836b25d070
gavin adlan hidayat <gavinadlan@gmail.com>
2025-03-27 00:31:22 +0700

    Fixed Logo

--- frontend/travel-story-app/src/components/Navbar.jsx
+++ frontend/travel-story-app/src/components/Navbar.jsx
@@ -35,8 +35,12 @@ const Navbar = ({
   };
 
   return (
-    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
-      <img src={LOGO} alt="travel story" className="h-9" />
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10 h-[60px]">
+      <img
+        src={LOGO}
+        alt="travel story"
+        className="h-[150px] w-auto object-contain"
+      />
 
       {isToken && (
         <>
