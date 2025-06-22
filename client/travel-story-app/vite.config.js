0b3a869bceda6a3c4a51052cd675ccc657e525c9
giep213 <gavinadlan@gmail.com>
2025-06-23 01:09:56 +0700

    chore(env): add Cloudinary env variables to Vite config

--- client/travel-story-app/vite.config.js
+++ client/travel-story-app/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": {
+      CLOUDINARY_CLOUD_NAME: JSON.stringify(process.env.CLOUDINARY_CLOUD_NAME),
+      CLOUDINARY_API_KEY: JSON.stringify(process.env.CLOUDINARY_API_KEY),
+    },
+  },
+});
